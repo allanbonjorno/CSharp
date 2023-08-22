22da59ffb8d5ffc223627f90bec6aa7c69ed6217
allanbonjorno <allanprofessional@outlook.com>
2023-08-22 11:09:33 -0300

    Aula 01, Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("A Turma 03 .NET É TOP");
+        }
+    }
+}
