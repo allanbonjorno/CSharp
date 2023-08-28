79b3eed51750b30783a3c3b7c7178d9e42baed34
allanbonjorno <allanprofessional@outlook.com>
2023-08-28 16:00:09 -0300

    Exercícios dia 28deAGOSTO

---
+++ aula05/vetores/Aula05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Design;
+
+namespace Aula05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor01 = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+            int busca = 0;
+
+
+
+            for (int indice = 0; indice < vetor01.Length; indice++)
+            { }
+            Console.WriteLine("\nDigite o número que deseja saber a posição no vetor : ");
+            
+            busca = Convert.ToInt32(Console.ReadLine());
+
+
+                if (Array.IndexOf(vetor01, busca) == -1)
+            {
+                Console.WriteLine($"O número {busca} não foi encontrado");
+            }
+            else
+            {
+                Console.WriteLine($"A Posição de {busca} é igual a: " + Array.IndexOf(vetor01, busca));
+            }
+
+
+
+
+
+
+
+        }
+    }
+    }
---
+++ aula05/vetores/ex2AULA05/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ex2AULA05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+            int somaPrin = 0, somaSec = 0;
+
+            for (int linha = 0; linha < Math.Sqrt(matriz.Length); linha++)
+            {
+                for (int coluna = 0; coluna < Math.Sqrt(matriz.Length); coluna++)
+                {
+                    Console.WriteLine($"Informe os números para preencher a matriz :  ");
+                    matriz[coluna, linha] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int linha = 0; linha < Math.Sqrt(matriz.Length); linha++)
+            {
+                for (int coluna = 0; coluna < Math.Sqrt(matriz.Length); coluna++)
+                {
+                    if (linha == coluna)
+
+                    {
+                        Console.WriteLine($"A diagonal Principal é: {matriz[linha, coluna]} ");
+                        somaPrin += matriz[linha, coluna];
+                    }
+
+
+                }
+            }
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matriz.GetLength(1); coluna++)
+                {
+                    if ((coluna + linha) == (matriz.GetLength(0) - 1))
+                    {
+                        Console.WriteLine($"A diagonal Secundária é ={matriz[linha, coluna]}");
+                        somaSec += matriz[linha, coluna];
+                    }
+                }
+            }
+            Console.WriteLine($"Resultado da soma Principal:{somaPrin}\nResultado da soma Secundária: {somaSec} ");
+
+
+
+        }
+
+
+
+
+    }
+}
