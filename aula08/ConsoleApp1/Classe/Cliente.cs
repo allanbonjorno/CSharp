1314bae2c3e469c0fecdf28a1a054667b98a7a1f
allanbonjorno <allanprofessional@outlook.com>
2023-08-31 15:52:42 -0300

    Commited

---
+++ aula08/ConsoleApp1/Classe/Cliente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.NovaPasta
+{
+    public class Cliente
+    {
+        private string cpf;
+        private float altura;
+        private int idade;
+        private string? nome;
+        private string? sexo;
+        public Cliente(string cpf, float altura, int idade, string nome, string sexo)
+        {
+            this.cpf = cpf;
+            this.altura = altura;
+            this.idade = idade;
+            this.nome = nome;
+            this.sexo = sexo;
+        }
+
+        public Cliente()
+        {
+        }
+
+        public string getCpf() { return cpf; }
+        public void setCpf(string cpf) { this.cpf = cpf; }
+        public float getAltura() { return altura; }
+        public void setAltura(float altura) { this.altura = altura; }
+        public int getIdade() { return idade; }
+        public void setIdade(int idade) { this.idade = idade; }
+        public string getNome() { return nome; }
+        public void setNome(string nome) { this.nome = nome; }
+        public string getSexo() { return sexo; }
+        public void setSexo(string sexo) { this.sexo = sexo; }
+
+        
+        
+                 public void Visualizar()
+        {
+  
+            
+            Console.WriteLine($"Nome: {this.nome}         ");
+            Console.WriteLine($"Gênero:{this.sexo}        ");
+            Console.WriteLine($"CPF:{this.cpf}            ");
+            Console.WriteLine($"Idade:{this.idade}        ");
+            Console.WriteLine($"Altura: {this.altura}     ");
+
+        }
+
+            }
+
+    }
+
+
+
+
+
---
+++ aula08/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1.NovaPasta;
+using System.Security.Cryptography;
+
+namespace DOCliente
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente CL1 = new Cliente("45707361812", 1.78F, 25, "Allan", "Masculino");
+            CL1.Visualizar();
+            Cliente CL2 = new Cliente("16555867606", 1.72F, 45, "Adailton", "Masculindo");
+            CL2.Visualizar();
+
+
+        }
+
+    }
+}
