3632828a7419192dc74f557770213d679d77139b
allanbonjorno <allanprofessional@outlook.com>
2023-08-31 15:57:04 -0300

    Commited

---
+++ aula08/ex06DIA31/AlunoEAD/ALUNOS.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex06DIA31.AlunoEAD
+{
+    internal class ALUNOS
+    {
+        private string nome;
+        private string cpf;
+        private int idade;
+        private string? sexo;
+        private int matricula;
+        public ALUNOS(string nome, string cpf, int idade, string sexo, int matricula)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.idade = idade;
+            this.sexo = sexo;
+            this.matricula = matricula;
+        }
+
+        public ALUNOS()
+        {
+        }
+
+        public string getNome() { return nome; }
+        public void setNome(string nome) { this.nome = nome; }
+        public string getCpf() { return cpf; }
+        public void setCpf(string cpf) { this.cpf = cpf; }
+        public int getIdade() { return idade; }
+        public void setIdade(int idade) { this.idade = idade; }
+        public string getSexo() { return sexo; }
+        public void setSexo(string sexo) { this.sexo = sexo; }
+        public int getMatricula() { return matricula; }
+        public void setMatricula(int matricula) { this.matricula = matricula; }
+
+
+
+        public void Visualizar()
+        {
+
+             
+            Console.WriteLine($"Nome: {this.nome}               ");
+            Console.WriteLine($"CPF:{this.cpf}                  ");
+            Console.WriteLine($"Gênero:{this.sexo}              ");
+            Console.WriteLine($"Idade:{this.idade}              ");
+            Console.WriteLine($"Matricula: {this.matricula}     ");
+
+        }
+    }
+}
---
+++ aula08/ex06DIA31/Program.cs
@@ -0,0 +1,15 @@
+﻿using ex06DIA31.AlunoEAD;
+
+namespace ex06DIA31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ALUNOS aL1 = new ALUNOS("ALLAN", "45707361812", 25, "MASCULINO", 310001);
+            aL1.Visualizar();
+            ALUNOS aL2 = new ALUNOS("ADAILTON", "165556676902", 45, "MASCULINO", 310002);
+            aL2.Visualizar();
+        }
+    }
+}
